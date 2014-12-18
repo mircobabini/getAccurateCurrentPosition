3169b3390cf0a478b4a1b988de77c0ae56f54855
Mirco Babini <mirkolofio@gmail.com>
2014-12-18 15:13:16 +0100

    Update geo.js
    I call this func just because I wanna have a maxWait set.
    I don't need the accuracy, so I don't pass it as an options and I want the enableHighAccuracy to stay asleep.

--- geo.js
+++ geo.js
@@ -11,7 +11,7 @@ navigator.geolocation.getAccurateCurrentPosition = function (geolocationSuccess,
         locationEventCount = locationEventCount + 1;
         // We ignore the first event unless it's the only one received because some devices seem to send a cached
         // location even when maxaimumAge is set to zero
-        if ((position.coords.accuracy <= options.desiredAccuracy) && (locationEventCount > 1)) {
+        if ( !options.enableHighAccuracy || (position.coords.accuracy <= options.desiredAccuracy) && (locationEventCount > 1)) {
             clearTimeout(timerID);
             navigator.geolocation.clearWatch(watchID);
             foundPosition(position);
@@ -35,12 +35,12 @@ navigator.geolocation.getAccurateCurrentPosition = function (geolocationSuccess,
         geolocationSuccess(position);
     };
 
+    options.maximumAge = 0; // Force current locations only
+    options.enableHighAccuracy = !!options.desiredAccuracy; // Maybe I just wanna take care about the maxWait, so..
+
     if (!options.maxWait)            options.maxWait = 10000; // Default 10 seconds
     if (!options.desiredAccuracy)    options.desiredAccuracy = 20; // Default 20 meters
     if (!options.timeout)            options.timeout = options.maxWait; // Default to maxWait
-
-    options.maximumAge = 0; // Force current locations only
-    options.enableHighAccuracy = true; // Force high accuracy (otherwise, why are you using this function?)
 
     watchID = navigator.geolocation.watchPosition(checkLocation, onError, options);
     timerID = setTimeout(stopTrying, options.maxWait); // Set a timeout that will abandon the location loop
